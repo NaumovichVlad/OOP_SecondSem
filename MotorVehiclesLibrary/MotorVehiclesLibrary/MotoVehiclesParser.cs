19f4dabc7822d2c5f5f5e071969f21688f9e5e4c
Naumovich <naumovichvlad.2001@gmail.com>
2021-02-11 21:26:23 +0300

    v0.1

---
+++ MotorVehiclesLibrary/MotorVehiclesLibrary/MotoVehiclesParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace MotorVehiclesLibrary
+{
+    public class MotoVehiclesParser
+    {
+        private List<MotorVehicle> vehicles;
+        private string fileXMLPath;
+        private string fileXMLSchemaPath;
+
+        public MotoVehiclesParser(string fileXMLPath, string fileXMLSchemaPath)
+        {
+            this.fileXMLPath = fileXMLPath;
+            this.fileXMLSchemaPath = fileXMLSchemaPath;
+        }
+        public MotorVehicle this[int index]
+        {
+            get { return vehicles[index]; }
+            set { vehicles[index] = value; }
+        }
+
+        public void ReadXML()
+        {
+
+        }
+    }
+}
